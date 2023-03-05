cc6a793fe83f3b4b3e82b2948c6b1e6da1f1a297
Owner <Owner@DESKTOP-D957Q9E>
2023-03-05 12:05:32 -0500

    Add project files.

---
+++ immutableID/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace recordDemo
+{
+    class Program
+    {
+        public record Student
+        {
+            public int Id { get; init; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            // default constructor
+            public Student()
+            {
+                Id = 0;
+                FirstName = "";
+                LastName = "";
+            }
+
+            public Student(int i)
+            {
+                Id = i;
+                FirstName = string.Empty;
+                LastName = string.Empty;
+            }
+            // parameterized constructor
+            public Student(int i, string First, string Last)
+            {
+                Id = i;
+                FirstName = First;
+                LastName = Last;
+            }
+        }
+        public static void Main()
+        {
+            Student student1 = new(34);
+            student1.FirstName = "John";
+            student1.LastName = "Wick";
+            Console.WriteLine(student1);
+            Student student2 = new(7, "James", "Bond");
+            Console.WriteLine(student2);
+        }
+
+    }
+}
